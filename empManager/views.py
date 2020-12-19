a9972ed9d151552647c056115f66c58223bd141a
keyurparalkar <keur.plkar@gmail.com>
2020-12-19 22:10:43 +0530

    Added Manage Page Template

--- empManager/views.py
+++ empManager/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import EmpLogin, EmpDetails
 
 # Create your views here.
 def emp_mng_view(request):
-
-    return HttpResponse("Hello world this is Employee Management page !!!")
+    x, y = EmpLogin.objects.all(), EmpDetails.objects.all()
+    data = {
+        'emp_emails':[i.emp_email for i in x],
+        'emp_details':[i for i in y]   
+    }
+    print(f"Data = {data['emp_details'][0] }")
+    return render(request, 'empManager/index.html', data)
 
 def add_emp_view(request):
     return HttpResponse("This is Add Employee Django view!!!!!")
