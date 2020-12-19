33a5542cdfd13e9af36627667710788d4bce29d0
keyurparalkar <keur.plkar@gmail.com>
2020-12-19 22:45:10 +0530

    Added Display Employee Details Page

--- empManager/urls.py
+++ empManager/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.emp_mng_view, name="manage_view"),
     path('add', views.add_emp_view, name="add_view"),
-    path('view', views.view_emp_view, name="display_view"),
+    path('view/<int:id>', views.display_emp_view, name="display_view"),
 ]
--- empManager/views.py
+++ empManager/views.py
@@ -1,19 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+
 from .models import EmpLogin, EmpDetails
 
 # Create your views here.
 def emp_mng_view(request):
-    x, y = EmpLogin.objects.all(), EmpDetails.objects.all()
-    data = {
-        'emp_emails':[i.emp_email for i in x],
-        'emp_details':[i for i in y]   
-    }
-    print(f"Data = {data['emp_details'][0] }")
+    try:
+        data = {
+            'emp_emails':[i.emp_email for i in EmpLogin.objects.all()],
+            'emp_details':[i for i in EmpDetails.objects.all()]   
+        }
+    except EmpLogin.DoesNotExist:
+        raise Http404("Employee Login does not exists")
+    except EmpDetails.DoesNotExist:
+        raise Http404("Employee Details not found")
+    
     return render(request, 'empManager/index.html', data)
 
 def add_emp_view(request):
     return HttpResponse("This is Add Employee Django view!!!!!")
 
-def view_emp_view(request):
-    return HttpResponse("This is View Employee Django view!!!!!")
+def display_emp_view(request, id):
+    try:
+        emp_info = EmpDetails.objects.get(pk=id)
+        info = {'emp_info': emp_info}
+    except EmpDetails.DoesNotExist:
+        raise HttpResponse("Employee Details does not exists")
+
+    return render(request, "empManager/display_details.html", info)
