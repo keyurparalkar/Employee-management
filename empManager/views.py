fb9d0fbcfef2f3e5d2354e6dad080da7013f9ed3
keyurparalkar <keur.plkar@gmail.com>
2020-12-19 19:37:39 +0530

    Created Data Model for Employee management

--- empManager/views.py
+++ empManager/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def emp_mng_view(request):
+
     return HttpResponse("Hello world this is Employee Management page !!!")
 
 def add_emp_view(request):
